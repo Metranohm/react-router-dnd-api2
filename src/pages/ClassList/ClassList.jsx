ee7893a9430d309fedd80be6ee43c9ac140bdd61
Ben Manley <benjamintmanley@gmail.com>
2022-10-11 15:13:58 -0500

    implement API call for ClassList data

--- src/pages/ClassList/ClassList.jsx
+++ src/pages/ClassList/ClassList.jsx
@@ -1,11 +1,38 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { getClassList } from "../../services/api-calls"
 
 const ClassList = () => {
-  const [classes, setClasses] = useState(['Paladin', 'Rogue', 'Wizard'])
+  const [classes, setClasses] = useState([])
+
+  useEffect(()=> {
+    const fetchClassData = async () => {
+      const classData = await getClassList()
+      setClasses(classData.results)
+    }
+    fetchClassData()
+  })
+
+
   return (
     <>
       <div>
-        <h3>I'm the class list!</h3>
+        <h3>Class List</h3>
+        <div className="icon-container">
+          {/** 
+						* This is where we map over our results, representing 
+						* each with a div containing an image and a name 
+					*/}
+          {classes.map(classTitle => 
+            <div className="class-div" key={classTitle.name}>
+              <img 
+								style={{ width: "100px", height: "100px" }}
+								src={`/images/${classTitle.name}.svg`} 
+								alt="class-logo"
+							/>
+              {classTitle.name}
+            </div>
+          )}
+        </div>
       </div>
     </>
   )
